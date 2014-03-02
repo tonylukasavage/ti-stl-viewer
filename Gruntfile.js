bfd01e394e89e4e9ce15f0fa516066c21f5152e2
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-03-02 17:18:56 -0500

    restructure, added test app

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,9 @@
+var path = require('path'),
+	wrench = require('wrench');
+
+var NAME = 'ti-stl-viewer',
+	TMP_DIR = 'tmp';
+
 module.exports = function(grunt) {
 
 	// Project configuration.
@@ -15,16 +21,33 @@ module.exports = function(grunt) {
 			options: {
 				jshintrc: true
 			},
-			tests: {
+			src: ['**/*.js']
+		},
+		titanium: {
+			create: {
 				options: {
-					jshintrc: true
-				},
-				src: ['test/*_test.js']
+					command: 'create',
+					name: TMP_DIR,
+					workspaceDir: '.'
+				}
 			},
-			src: ['lib/*.js']
+			build: {
+				options: {
+					command: 'build',
+					projectDir: TMP_DIR
+				}
+			}
+		},
+		alloy: {
+			all: {
+				options: {
+					command: 'new',
+					args: [TMP_DIR]
+				}
+			}
 		},
 		clean: {
-			src: ['tmp']
+			src: [TMP_DIR]
 		}
 	});
 
@@ -32,6 +55,30 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-titanium');
+	grunt.loadNpmTasks('grunt-alloy');
+
+	// load test app
+	grunt.registerTask('load-app', 'Load source files into example alloy app', function() {
+		var srcDir = path.join('test', 'app'),
+			dstDir = path.join(TMP_DIR, 'app'),
+			widgetsDir = path.join(dstDir, 'widgets', NAME);
+
+		// copy app source files
+		grunt.log.write('Copying "%s" to "%s"... ', srcDir, dstDir);
+		wrench.copyDirSyncRecursive(srcDir, dstDir, { forceDelete: true });
+		grunt.log.ok();
+
+		// copy in widget
+		grunt.log.write('Copying %s widget to "%s"... ', NAME, widgetsDir);
+		wrench.mkdirSyncRecursive(widgetsDir);
+		wrench.copyDirSyncRecursive(NAME, widgetsDir, { forceDelete: true });
+		grunt.log.ok();
+
+	});
+
+	// run example app
+	grunt.registerTask('test-run', ['clean', 'titanium:create', 'alloy', 'load-app', 'titanium:build']);
 
 	// run tests
 	grunt.registerTask('test', ['mochaTest', 'clean']);
